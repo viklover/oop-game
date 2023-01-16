d836c6709f73abc5f43ab0638cf8ad1abfe48ee5
Mihail Vorobyev <1viklover@gmail.com>
2023-01-16 14:10:32 +0500

    Initial commit

---
+++ src/main/java/ru/viklover/oopgame/OopGameApplication.java
@@ -0,0 +1,13 @@
+package ru.viklover.oopgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/viklover/oopgame/OopGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.viklover.oopgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
